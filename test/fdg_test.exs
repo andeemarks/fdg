0d0859658c30cca652f7fb02f42ebccfc94d31e7
Andy Marks <amarks@thoughtworks.com>
2016-06-06 16:59:27 +1000

    Adding tests for invalid input

--- test/fdg_test.exs
+++ test/fdg_test.exs
@@ -7,7 +7,16 @@ defmodule FDGTest do
   test "input parsing can handle dependencies" do
     assert Map.fetch!(FDG.parse("(A,B,C,G,H)[G->A,H->A,H->B]"), :deps) == ["G->A", "H->A", "H->B"]
   end
+  
   test "input parsing can handle features across multiple specs" do
     assert Map.fetch!(FDG.parse("(A,B,C,G,H)[G->A,H->A,H->B]\n(D,E,F,I,J)[I->D,I->E,J->F,J->I,I->H]"), :features) == ["A", "B", "C", "G", "H", "D", "E", "F", "I", "J"]
+  end
+
+  test "dependency parsing fails if source feature does not exist" do
+    assert_raise RuntimeError, fn -> FDG.parse("(G)[G->A]") end
+  end
+
+  test "dependency parsing fails if destination feature does not exist" do
+    assert_raise RuntimeError, fn -> FDG.parse("(G)[G->A]") end
   end
 end
